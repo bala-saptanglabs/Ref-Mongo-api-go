ebc2954b606e61d9ee7be708309c00632bdea522
bala-saptanglabs <balamurugan@saptanglabs.com>
2023-06-28 18:05:38 +0530

    commit made

--- main.go
+++ main.go
@@ -9,6 +9,14 @@ to kill a process
 lsof -i tcp:port_number
 kill -9 p_id
 
+Run
+
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+
+to set your account's default identity.
+Omit --global to set the identity only in this repository.~
+
 */
 
 import (
